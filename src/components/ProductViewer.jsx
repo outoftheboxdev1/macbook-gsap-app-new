2aa88cff41561125a1d713da566130148e75cfbe
outoftheboxdev1 <130626635+outoftheboxdev1@users.noreply.github.com>
2025-10-17 00:39:15 -0400

    Lighting Adjustments

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -4,6 +4,7 @@ import clsx from "clsx";
 import {Canvas} from "@react-three/fiber";
 import {Box, OrbitControls} from "@react-three/drei";
 import MacbookModel14 from "./models/Macbook-14.jsx";
+import StudioLights from "./StudioLights.jsx";
 
 const ProductViewer = () => {
     const { color, scale, setColor, setScale } = useMacbookStore();
@@ -41,6 +42,7 @@ const ProductViewer = () => {
                 </div>
             </div>
             <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
+                <StudioLights />
                 <ambientLight intensity={1} />
                 <MacbookModel14 scale={0.06} positon={[0, 0, 0]} />
 
