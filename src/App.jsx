e6d731ca8bece3f759ca3d36832fc9f7ac279009
outoftheboxdev1 <130626635+outoftheboxdev1@users.noreply.github.com>
2025-10-16 21:53:45 -0400

    Navbar & Styling Edits
    Navbar Integration, Styling, and Images.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import NavBar from "./components/NavBar.jsx";
 
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline text-indigo-700">
-            Hello world!
-        </h1>
+        <main>
+            <NavBar />
+        </main>
     )
 }
 export default App
