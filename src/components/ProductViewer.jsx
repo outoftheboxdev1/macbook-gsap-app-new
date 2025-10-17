9964a1576f659f62366a2da1d853ecb9a3dc2621
outoftheboxdev1 <130626635+outoftheboxdev1@users.noreply.github.com>
2025-10-17 02:15:07 -0400

    Model Switch Logic
    Added logic for model switching with smooth animations, some bug fixes, and some lighting adjustments.

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -4,15 +4,19 @@ import clsx from "clsx";
 import {Canvas} from "@react-three/fiber";
 import {Box, OrbitControls} from "@react-three/drei";
 import MacbookModel14 from "./models/Macbook-14.jsx";
-import StudioLights from "./StudioLights.jsx";
+import StudioLights from "./three/StudioLights.jsx";
+
+import ModelSwitcher from "./three/ModelSwitcher.jsx";
+import {useMediaQuery} from "react-responsive";
 
 const ProductViewer = () => {
     const { color, scale, setColor, setScale } = useMacbookStore();
+    const isMobile = useMediaQuery({ query: '(max-width: 1024px)' });
     return (
         <section id="product-viewer">
             <h2>Take a closer look.</h2>
             <div className="controls">
-                <p className="info">MacbookPro {scale} in {color}</p>
+                <p className="info">MacbookPro | Available in 14" & 16" in Space Gray & Dark colors</p>
                 <div className="flex-center gap-5 mt-5">
                     <div className="color-control">
                         <div
@@ -43,10 +47,8 @@ const ProductViewer = () => {
             </div>
             <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
                 <StudioLights />
-                <ambientLight intensity={1} />
-                <MacbookModel14 scale={0.06} positon={[0, 0, 0]} />
 
-                <OrbitControls enableZoom={false} />
+                <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
             </Canvas>
         </section>
     )
